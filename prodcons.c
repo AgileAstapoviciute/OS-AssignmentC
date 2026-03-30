b46af3d6877b0c4328fe7bc95de8e1fc1b41747a
AgileAstapoviciute <a.astapoviciute@student.tue.nl>
2026-03-30 08:46:56 -0700

    Added student numbers

--- prodcons.c
+++ prodcons.c
@@ -2,8 +2,9 @@
  * Operating Systems  (2INC0)  Practical Assignment.
  * Condition Variables Application.
  *
- * STUDENT_NAME_1 (STUDENT_NR_1)
- * STUDENT_NAME_2 (STUDENT_NR_2)
+ * Cunyuan Liu 2330512
+ * Christina Tsakloglou 2413094
+ * Agile Astapoviciute 1962965
  *
  * Grading:
  * Students who hand in clean code that fully satisfies the minimum requirements will get an 8.
